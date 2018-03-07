42558e48cc37dd914345e3dcd485bb20df785109
psulzycki <sulzyckipawel@gmail.com>
2018-03-07 22:22:08 +0100

    created project

---
+++ PgsKanban_Backend/Pgs.Kanban.Api/Pgs.Kanban.Api/Controllers/RandomGeneratorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pgs.Kanban.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pgs.Kanban.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RandomGeneratorController : Controller
+    {
+        private RandomGeneratorService _randomGeneratorService;
+
+        public RandomGeneratorController()
+        {
+            _randomGeneratorService = new RandomGeneratorService();
+        }
+
+        [HttpGet]
+        //[Route("[action]")]
+        public IActionResult GetRandomNumber()
+        {
+            return Ok(_randomGeneratorService.GenerateRandomNumber());
+        }
+
+        [HttpGet]
+        [Route("{maxValue}")]
+        public IActionResult GetRandomNumberInRange(int maxValue)
+        {
+            return Ok(_randomGeneratorService.GenerateRandomNumber(maxValue));
+        }
+
+        [HttpPost]
+        public IActionResult AddNumber([FromBody]int number)
+        {
+            _randomGeneratorService.AddNumberToList(number);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteNumber(int number)
+        {
+            _randomGeneratorService.DeleteNumber(number);
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("AllNumbers")]
+        public IActionResult GetAllNumbers()
+        {
+            return Ok(_randomGeneratorService.GetAllNumbers());
+        }
+    }
+}
---
+++ PgsKanban_Backend/Pgs.Kanban.Api/Pgs.Kanban.Domain/Services/RandomGeneratorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pgs.Kanban.Domain.Services
+{
+    public class RandomGeneratorService
+    {
+        private Random _randomGenerator = new Random();
+        private static List<int> _magicNumber = new List<int>();
+
+        public int GenerateRandomNumber()
+        {
+            var number = _randomGenerator.Next();
+            return number;
+        }
+
+        public int GenerateRandomNumber(int maxValue)
+        {
+            var number = _randomGenerator.Next(maxValue);
+            return number;
+        }
+
+        public void AddNumberToList(int number)
+        {
+            _magicNumber.Add(number);
+        }
+
+        public void DeleteNumber(int number)
+        {
+            _magicNumber.Remove(number);
+        }
+
+        public List<int> GetAllNumbers()
+        {
+            return _magicNumber;
+        }
+    }
+}
