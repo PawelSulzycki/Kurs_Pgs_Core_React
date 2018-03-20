47e46cac88d9fd6e0310e258188b1f9f9a8c245b
psulzycki <sulzyckipawel@gmail.com>
2018-03-20 16:42:35 +0100

    work for lesson in PGS

---
+++ PgsKanban_Backend/Pgs.Kanban.Api/Pgs.Kanban.Api/Controllers/BoardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pgs.Kanban.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pgs.Kanban.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BoardController : Controller
+    {
+        private readonly BoardService _boardService;
+
+        public BoardController()
+        {
+            _boardService = new BoardService();
+        }
+
+        [HttpGet]
+        public IActionResult GetBoard()
+        {
+            var response = _boardService.GetBoard();
+
+            if(response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ PgsKanban_Backend/Pgs.Kanban.Api/Pgs.Kanban.Domain/Services/BoardService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pgs.Kanban.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pgs.Kanban.Domain.Services
+{
+    public class BoardService
+    {
+        private readonly KanbanContext _context;
+
+        public BoardService()
+        {
+            _context = new KanbanContext();
+        }
+
+        public BoardDto GetBoard()
+        {
+            var board = _context.Boards
+                .Include(b => b.Lists)
+                .FirstOrDefault();
+
+            //singleOrDefault wyrzuci wyjatek jak jest wiecej niz jeden, stosuje sie do id
+
+            if (board == null)
+            {
+                return null;
+            }
+
+            //auto maper
+
+            var boardDto = new BoardDto()
+            {
+                Id = board.Id,
+                Name = board.Name,
+                Lists = board.Lists.Select(l => new ListDto()
+                {
+                    Id = l.Id,
+                    Name = l.Name,
+                    BoardId = l.BoardId
+                }).ToList()
+            };
+
+            return boardDto;
+        }
+    }
+}
